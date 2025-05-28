c7dcadca7e32c5cc09db0668afe96ab99ecf66b8
Cristian Cioanca <ccioanca@users.noreply.github.com>
2025-05-27 23:50:50 -0400

    Added All Visual Assets

--- GameManager.cs
+++ GameManager.cs
@@ -9,21 +9,25 @@ using Tankathon.EvilTank;
 namespace Tankathon.API.Internal;
 public partial class GameManager : Node2D
 {
-	[ExportGroup("TL Tank")]
+	//TODO: Actually name all these resources based on the team names
+	[ExportGroup("R1 Fight")]
 	[Export]
-	Resource tlTankRes;
+	Resource tlTankResR1;
+	Resource trTankResR1;
+	Resource blTankResR1;
+	Resource brTankResR1;
 
-	[ExportGroup("TR Tank")]
-	[Export]
-	Resource trTankRes;
+	// [ExportGroup("R2 Fight")]
+	// [Export]
+	// Resource trTankRes;
 
-	[ExportGroup("BL Tank")]
-	[Export]
-	Resource blTankRes;
+	// [ExportGroup("R3 Fight")]
+	// [Export]
+	// Resource blTankRes;
 
-	[ExportGroup("BR Tank")]
-	[Export]
-	Resource brTankRes;
+	// [ExportGroup("R4 Fight")]
+	// [Export]
+	// Resource brTankRes;
 
 	TheTank tlTank = null;
 	TheTank trTank = null;
@@ -36,13 +40,51 @@ public partial class GameManager : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		//initialize the current fight here
+		//initialize the fights here
+		//Test Fight
 		_tankTypes = new List<TeamModel>
 		{
-			new TeamModel(typeof(DumTank), tlTankRes)
+			new TeamModel(typeof(DumTank), tlTankResR1),
+			new TeamModel(typeof(DumTank), tlTankResR1),
+			new TeamModel(typeof(DumTank), tlTankResR1),
+			new TeamModel(typeof(DumTank), tlTankResR1)
 		};
 
+		//Fight 1
+		// _tankTypes = new List<TeamModel>
+		// {
+		// 	new TeamModel(typeof(DumTank), tlTankResR1),
+		// 	new TeamModel(typeof(DumTank), trTankResR1),
+		// 	new TeamModel(typeof(DumTank), blTankResR1),
+		// 	new TeamModel(typeof(DumTank), brTankResR1)
+		// };
 
+		//Fight 2
+		// _tankTypes = new List<TeamModel>
+		// {
+		// 	new TeamModel(typeof(DumTank), tlTankRes),
+		// 	new TeamModel(typeof(DumTank), tlTankRes),
+		// 	new TeamModel(typeof(DumTank), tlTankRes),
+		// 	new TeamModel(typeof(DumTank), tlTankRes)
+		// };
+
+		//Fight 3
+		// _tankTypes = new List<TeamModel>
+		// {
+		// 	new TeamModel(typeof(DumTank), tlTankRes),
+		// 	new TeamModel(typeof(DumTank), tlTankRes),
+		// 	new TeamModel(typeof(DumTank), tlTankRes),
+		// 	new TeamModel(typeof(DumTank), tlTankRes)
+		// };
+
+		//Fight 4
+		// _tankTypes = new List<TeamModel>
+		// {
+		// 	new TeamModel(typeof(DumTank), tlTankRes),
+		// 	new TeamModel(typeof(DumTank), tlTankRes),
+		// 	new TeamModel(typeof(DumTank), tlTankRes),
+		// 	new TeamModel(typeof(DumTank), tlTankRes)
+		// };
 
 		tlTank = GetNode<TheTank>("TopLeftTank");
 		trTank = GetNode<TheTank>("TopRightTank");
@@ -51,13 +93,13 @@ public partial class GameManager : Node2D
 
 
 		tlTank.thisTank = Activator.CreateInstance(_tankTypes[0].tankType) as ITank;
-		trTank.thisTank = Activator.CreateInstance(_tankTypes[0].tankType) as ITank;
-		blTank.thisTank = Activator.CreateInstance(_tankTypes[0].tankType) as ITank;
-		brTank.thisTank = Activator.CreateInstance(_tankTypes[0].tankType) as ITank;
+		trTank.thisTank = Activator.CreateInstance(_tankTypes[1].tankType) as ITank;
+		blTank.thisTank = Activator.CreateInstance(_tankTypes[2].tankType) as ITank;
+		brTank.thisTank = Activator.CreateInstance(_tankTypes[3].tankType) as ITank;
 
 		tlTank.Init(_tankTypes[0].tankInfo.Get("shootSound").As<AudioStream>(), _tankTypes[0].tankInfo.Get("deathSound").As<AudioStream>());
-		trTank.Init();
-		blTank.Init();
-		brTank.Init();
+		trTank.Init(_tankTypes[1].tankInfo.Get("shootSound").As<AudioStream>(), _tankTypes[0].tankInfo.Get("deathSound").As<AudioStream>());
+		blTank.Init(_tankTypes[2].tankInfo.Get("shootSound").As<AudioStream>(), _tankTypes[0].tankInfo.Get("deathSound").As<AudioStream>());
+		brTank.Init(_tankTypes[3].tankInfo.Get("shootSound").As<AudioStream>(), _tankTypes[0].tankInfo.Get("deathSound").As<AudioStream>());
 	}
 }
--- Scripts/team_info.gd
+++ Scripts/team_info.gd
@@ -2,6 +2,5 @@
 extends Resource
 class_name TeamInfo
 
-@export var teamName: String
 @export var shootSound: AudioStream
 @export var deathSound: AudioStream
