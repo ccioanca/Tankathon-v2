e509f76477af3373ef7e877a4c2db8b6e3d6f282
Cristian Cioanca <ccioanca@users.noreply.github.com>
2025-05-30 21:35:50 -0400

    Revamped how Battles are defined
    - Removed Team Model CS
    - Removed Battle Info GD
    - Added Team Data CS
    - Added Battle Info CS
    - Recreated the resources as needed for testing

--- GameManager.cs
+++ GameManager.cs
@@ -1,19 +1,23 @@
 using Godot;
 using Godot.Collections;
+using Microsoft.VisualBasic;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Reflection;
 using Tankathon.API;
 using Tankathon.EvilTank;
+using Tankathon.Scripts;
 
 namespace Tankathon.API.Internal;
+
 public partial class GameManager : Node2D
 {
 	//TODO: Actually name all these resources based on the team names
 	[ExportGroup("R1 Fight")]
 	[Export]
-	Resource r1Battle;
+	BattleInfo r1Battle;
 
 	// [ExportGroup("R2 Fight")]
 	// [Export]
@@ -33,19 +37,19 @@ public partial class GameManager : Node2D
 	TheTank brTank = null;
 
 	//list of combatants
-	private List<TeamModel> _tankTypes;
+	private List<TeamData> _tankTypes;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		//initialize the fights here
 		//Test Fight
-		_tankTypes = new List<TeamModel>
+		_tankTypes = new List<TeamData>
 		{
-			new TeamModel(typeof(DumTank), r1Battle.Get("teams").As<Godot.Collections.Array>()[0].As<Godot.Resource>()),
-			new TeamModel(typeof(DumTank), r1Battle.Get("teams").As<Godot.Collections.Array>()[1].As<Godot.Resource>()),
-			new TeamModel(typeof(DumTank), r1Battle.Get("teams").As<Godot.Collections.Array>()[2].As<Godot.Resource>()),
-			new TeamModel(typeof(DumTank), r1Battle.Get("teams").As<Godot.Collections.Array>()[3].As<Godot.Resource>())
+			r1Battle.Get("teams").As<Godot.Collections.Array>()[0].As<TeamData>(),
+			r1Battle.Get("teams").As<Godot.Collections.Array>()[1].As<TeamData>(),
+			r1Battle.Get("teams").As<Godot.Collections.Array>()[2].As<TeamData>(),
+			r1Battle.Get("teams").As<Godot.Collections.Array>()[3].As<TeamData>()
 		};
 
 		tlTank = GetNode<TheTank>("TopLeftTank");
@@ -54,18 +58,19 @@ public partial class GameManager : Node2D
 		blTank = GetNodeOrNull<TheTank>("BottomLeftTank");
 
 
-		tlTank.thisTank = Activator.CreateInstance(_tankTypes[0].tankType) as ITank;
-		brTank.thisTank = Activator.CreateInstance(_tankTypes[1].tankType) as ITank;
-		if(trTank != null)
-			trTank.thisTank = Activator.CreateInstance(_tankTypes[2].tankType) as ITank;
-		if(blTank != null)
-			blTank.thisTank = Activator.CreateInstance(_tankTypes[3].tankType) as ITank;
+		tlTank.thisTank = Activator.CreateInstance(Type.GetType(_tankTypes[0].tankType)) as ITank;
+		brTank.thisTank = Activator.CreateInstance(Type.GetType(_tankTypes[0].tankType)) as ITank;
+		if (trTank != null)
+			trTank.thisTank = Activator.CreateInstance(Type.GetType(_tankTypes[0].tankType)) as ITank;
+		if (blTank != null)
+			blTank.thisTank = Activator.CreateInstance(Type.GetType(_tankTypes[0].tankType)) as ITank;
 
-		tlTank.Init(_tankTypes[0].tankInfo);
-		brTank.Init(_tankTypes[1].tankInfo);
-		if(trTank != null)
-			trTank.Init(_tankTypes[2].tankInfo);
-		if(blTank != null)
-			blTank.Init(_tankTypes[3].tankInfo);
+		tlTank.Init(_tankTypes[0]);
+		brTank.Init(_tankTypes[1]);
+		if (trTank != null)
+			trTank.Init(_tankTypes[2]);
+		if (blTank != null)
+			blTank.Init(_tankTypes[3]);
 	}
+	
 }
