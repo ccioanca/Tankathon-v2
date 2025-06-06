4028aaa69b579b15c7e6e5c90214ae5e07d4c94a
Cristian Cioanca <ccioanca@users.noreply.github.com>
2025-06-05 23:36:36 -0400

    Tons of stuff

--- API/Bullet.cs
+++ API/Bullet.cs
@@ -8,6 +8,16 @@ public partial class Bullet : Area2D, IEntity
     public EntityType eType => EntityType.Bullet;
 
 	public TheTank initializer;
+	
+	private CpuParticles2D particles;
+    private PackedScene explosion;
+	
+	public override void _Ready()
+	{
+		particles = GetNode<CpuParticles2D>("Trail");
+		explosion = GD.Load<PackedScene>("res://Scenes/Explosion.tscn");
+		base._Ready();
+	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _PhysicsProcess(double delta)
@@ -22,10 +32,20 @@ public partial class Bullet : Area2D, IEntity
 			if (body is TheTank)
 			{
 				(body as TheTank).Hurt();
-				initializer.Score();
 			}
-            initializer.PopBullet(this);
+			particles.Reparent(GetParent());
+			particles.Emitting = false;
+			Explode();
+			
 			QueueFree();
 		}
+    }
+
+	public void Explode()
+	{
+		Node2D explInstance = explosion.Instantiate<Node2D>();
+		explInstance.Position = GlobalPosition;
+		explInstance.Rotation = Rotation;
+        GetParent().AddChild(explInstance);
     }
 }
